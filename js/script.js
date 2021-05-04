5ceb31f7172096383ab1a26ba47a5285f7b2d0f7
juandab <juandarure@gmail.com>
2021-05-04 01:34:06 -0500

    version beta-2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 function iniciarMap(){
     var coord = {lat:4.6019163105637855 ,lng: -74.07839655898347};
     var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 12,
+      zoom: 14,
       center: coord
     });
     var marketOne = {lat:4.600026479460904, lng:-74.07999119871776};
@@ -20,5 +20,24 @@ function iniciarMap(){
         position: MarketTwo,
         map: map
     });
+    var MarketFour = {lat:4.602112,lng:-74.076846};
+    var marker = new google.maps.Marker({
+      position: MarketFour,
+      map: map 
+    })
 };
-    
+  
+var reproductor = document.getElementById("reproducir");
+reproducir.addEventListener('click',presionar,false);
+function presionar(){
+  if (!medio.paused && !medio.ended) {
+    medio.paused();
+    reproducir.innerHTML='Reproducir';
+    window.clearInterval(bucle);
+  }else{
+    medio.play();
+    reproducir.innerHTML='Pausa';
+    bucle.setInterval(estado, 1000);
+  }
+
+};
